50cecd4987ff661190186bea101ce0067f895687
turduhodzhaev99 <turduhodzhaev99@gmail.com>
2020-07-17 11:49:07 +0600

    first commit

---
+++ task10.py
@@ -0,0 +1,9 @@
+
+#Task 5
+
+#Given a string consisting of words separated by spaces. 
+#Determine how many words it has. To solve the problem, use the method count.
+a = "aknf ksjf sdnf kjns lkj dfn"
+print(a.count(" ")+1)
+
+
